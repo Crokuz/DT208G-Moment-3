0b832021d52b0dfe6807c7ee212939358476099c
Crokuz <leander.norberg@hotmail.com>
2025-10-21 02:36:21 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About } from './pages/about/about';
+import { Converter } from './pages/converter/converter';
+import { Environment } from './pages/environment/environment';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "home", component: Home},
+    {path: "about", component: About},
+    {path: "converter", component: Converter},
+    {path: "environment", component: Environment},
+    {path: "", redirectTo: "home", pathMatch: "full"}
+];
